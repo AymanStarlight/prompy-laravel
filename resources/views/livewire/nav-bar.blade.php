3457c270cdbc78fc71737a11427abf446ae3451b
Ayman Ait Laaguid <laaguid.ayman@gmail.com>
2023-09-24 12:23:48 +0100

    ProjectInit / Livewire/ NavBarStart / GeneralStyle

---
+++ resources/views/livewire/nav-bar.blade.php
@@ -0,0 +1,11 @@
+<nav className="flex-between w-full mb-16 pt-3">
+    <a href={{ route('home.index') }} class="flex gap-2 flex-center">
+        <img
+            src={{ asset("storage/assets/images/logo-robot.svg") }}
+            alt="Prompy Logo"
+            width="45"
+            height="45"
+            class="object-contain"
+        />
+    </a>
+</nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,14 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                satoshi: ['Satoshi', 'sans-serif'],
+                inter: ['Inter', 'sans-serif'],
             },
+            colors: {
+                'primary-red': 'rgba(162,2,63,1)',
+                'primary-blue': '#3B4652',
+                'secondary-blue': '#c3cfe2'
+              }
         },
     },
 
