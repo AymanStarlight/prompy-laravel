820c2c012bfaf4d75721f7cafc5eb30a2e6f2b54
Ayman Ait Laaguid <laaguid.ayman@gmail.com>
2023-09-30 11:02:56 +0100

    Assets

--- routes/web.php
+++ routes/web.php
@@ -23,14 +23,14 @@ Route::get('/', function () {
     return redirect()->route('home.index');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    // Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    // Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Pages
     Route::get('/create', [HomeController::class, 'create'])->name('create.index');
